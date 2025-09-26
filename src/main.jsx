eacfa4430c0aa9c6dc874f4afaaf095a0ac22a17
= <chadwinwong@gmail.com>
2025-09-26 16:07:16 -0500

    Redirect to window origin

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: import.meta.env.VITE_COGNITO_AUTHORITY,
   client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
-  redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
+  redirect_uri: window.location.origin,
   response_type: "code",
   scope: "email openid phone"
 };
